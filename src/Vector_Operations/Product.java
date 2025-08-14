743867ae6b22936cb1d6e1740449073d933f493e
fajarkhurram <fajarkhurram0067@gmail.com>
2025-08-14 17:16:57 +0500

    Update Product.java

--- src/Vector_Operations/Product.java
+++ src/Vector_Operations/Product.java
@@ -1,8 +1,12 @@
 package Vector_Operations;
 
+import Vector_Points.Point;
 import Vector_Points.Vector2D;
 
+
 public class Product {
+    private Point p1;
+    private Point p2;
 
     //dot product of two vectors returns scalar
     public  double dotProduct(Vector2D v1 , Vector2D v2){
@@ -12,8 +16,9 @@ public class Product {
 
     //cross product: v1 x v2 = (v1.x * v2.y) - (v1.y * v2.x)
     // it also returns a scalar quantity
-    public  double crossProduct(Vector2D v1 , Vector2D v2){
-        double scalar  = (v1.getX()*v2.getY()) - (v1.getY()*v2.getX());
+    public  double crossProduct(Vector2D v1, Vector2D v2){
+//
+        double scalar =  (v1.getX()*v2.getY()) - (v1.getY()*v2.getX());
         return scalar;
     }
 }
